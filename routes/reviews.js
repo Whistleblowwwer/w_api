e2ec81dfc590bc44acc9f0b6c8fe5bec0ff50677
David <beltrangonzalezdavid@gmail.com>
2023-10-26 16:30:48 -0500

    Modifications to post review

--- routes/reviews.js
+++ routes/reviews.js
@@ -1,14 +1,12 @@
 import { Router } from "express";
 import { validateToken } from "../middlewares/jwt.js"; // Import the validateToken middleware
-import { 
-    createReview
-} from "../controllers/reviews.js";
+import { createReview } from "../controllers/reviews.js";
 
 const router = Router();
 
 //----------Reviews Routes-------------
 
 //Create Review
-router.post('/:_id_business', validateToken, createReview)
+router.post("/", validateToken, createReview);
 
 export default router;
