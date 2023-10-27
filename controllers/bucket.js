3baceaa076b2f409b7545d447e9b15fa9a98d91a
Ubername0 <santiago.remes@gmail.com>
2023-10-26 21:51:43 -0600

    Video y cambio a uploadFIle

--- controllers/bucket.js
+++ controllers/bucket.js
@@ -8,7 +8,7 @@ import { User } from "../models/users.js";
 import { Business } from "../models/business.js"
 import { Review } from "../models/reviews.js"
 
-export const uploadFile = async (req, res) => {
+export const uploadPhoto = async (req, res) => {
     try{
         const { id, photo_type } = req.query;
         upload.single('fileN')(req, res, async (err) => {
@@ -18,9 +18,9 @@ export const uploadFile = async (req, res) => {
             } else {
                 //Get image and check it is an image type file.
                 const contentType = req.file.mimetype;
-                if (contentType !== "image/jpeg") {
-                    return res.status(400).send({ message: "Unsupported Media Type" });
-                }
+                // if (contentType !== "image/jpeg") {
+                //     return res.status(400).send({ message: "Unsupported Media Type" });
+                // }
     
                 //Prepare file for bucket and send file.
                 const file = req.file.buffer;
@@ -94,7 +94,7 @@ export const uploadFile = async (req, res) => {
                     return res.status(400).send({ message: "No photo type specified" });
                 }
                 await s3.send(command);
-                return res.status(200).send({ message: "File Uploaded Successfully  "})
+                return res.status(200).send({ message: "Photo Uploaded Successfully  "})
             }
         });
     }
@@ -103,3 +103,94 @@ export const uploadFile = async (req, res) => {
     }
     
 }
+
+export const uploadVideo = async (req, res) => {
+    try{
+        const { id, video_type } = req.query;
+        upload.single('fileN')(req, res, async (err) => {
+            
+            if (err) {
+                return res.status(404).send({ message: "Internal Error" });
+            } else {
+                //Get image and check it is an image type file.
+                const contentType = req.file.mimetype;
+                // if (contentType !== "image/jpeg") {
+                //     return res.status(400).send({ message: "Unsupported Media Type" });
+                // }
+    
+                //Prepare file for bucket and send file.
+                const file = req.file.buffer;
+                const fileName = `${video_type}/${id}`;
+                const params = {
+                    Bucket: bucketName,
+                    Key: fileName,
+                    Body: file,
+                    ContentType: contentType
+                }
+    
+                const command = new PutObjectCommand(params);
+                await s3.send(command);
+                return res.status(200).send({ message: "Video Uploaded Successfully  "})
+            }
+        });
+    }
+    catch(error){
+        return res.status(500).send({ error: error.message });
+    }
+    
+}
+
+export const getFile = async (req, res) => {
+    const { id, photo_type } = req.query;
+    var file = null;
+
+    if(photo_type === "users_profile_img"){
+        const _id_user = id;
+        const user = await User.findOne({
+            where: { _id_user },
+            attributes: { exclude: ["password_token"] },
+        });
+        if (!user) {
+            return res.status(400).send({ message: "Business not found" });
+        }
+
+        file = user.profile_picture_url;
+        
+    }
+    else if(photo_type === "business_header_img"){
+        const _id_business = id;
+        const business = await Business.findOne({
+            where: { _id_business },
+        });
+        if (!business) {
+            return res.status(400).send({ message: "Business not found" });
+        }
+
+        file = business.profile_picture_url;
+        
+    }
+    else if(photo_type === "reviews_img"){
+        const _id_review = id;
+        const review = await Review.findOne({
+            where: { _id_review },
+        });
+        if (!review) {
+            return res.status(400).send({ message: "Business not found" });
+        }
+
+        file = review.image_url;
+        
+    }
+
+    const getObjectParams = {
+        Bucket: bucketName,
+        Key: file
+    }
+    const command = new GetObjectCommand(getObjectParams);
+    const url = await getSignedUrl(s3, command, { expiresIn: 3600 });
+    res.status(201).json({
+        fileName: file,
+        url: url
+    });
+}
+
--- routes/bucket.js
+++ routes/bucket.js
@@ -1,7 +1,9 @@
 import { Router } from "express";
 import { validateToken } from "../middlewares/jwt.js";
 import {
-    uploadFile,
+    uploadPhoto,
+    uploadVideo,
+    getFile
 } from "../controllers/bucket.js";
 
 const router = Router();
@@ -9,6 +11,8 @@ const router = Router();
 //----------Bucket Routes-------------
 
 //Add/Change Image to User/Buisiness/Review
-router.post("/",validateToken ,uploadFile);
+router.post("/", validateToken, uploadPhoto);
+router.post("/video", validateToken, uploadVideo);
+router.get("/", validateToken, getFile);
 
 export default router;
