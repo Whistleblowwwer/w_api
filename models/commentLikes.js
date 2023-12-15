58558c4ac92f77f865b4bea1877e46e2c81fd283
Alfonso García Yague <93610107+p0nch000@users.noreply.github.com>
2023-12-14 22:23:30 -0600

    Comment Likes fix

--- models/commentLikes.js
+++ models/commentLikes.js
@@ -1,13 +1,24 @@
 import { DataTypes } from "sequelize";
 import { sequelize } from "../config/db.js";
+import { User } from "./users.js"; 
+import { Comment } from "./comments.js"; 
 
 export const CommentLikes = sequelize.define(
     "CommentLikes",
     {
-        _id_comment_likes: {
+        _id_user: {
             type: DataTypes.UUID,
-            defaultValue: DataTypes.UUIDV4,
-            primaryKey: true,
+            references: {
+                Model: User, 
+                key: '_id_user',
+            },
+        },
+        _id_comment: {
+            type: DataTypes.UUID,
+            references: {
+                Model: Comment, 
+                key: '_id_comment',
+            },
         },
     },
     {
