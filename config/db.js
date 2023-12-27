471195e2d1dc3db65b378293f1ca4af60435e9e5
David <beltrangonzalezdavid@gmail.com>
2023-12-27 18:49:03 -0500

    fix DB connection

--- config/db.js
+++ config/db.js
@@ -4,12 +4,12 @@ import dotenv from "dotenv";
 dotenv.config();
 
 export const sequelize = new Sequelize(
-    process.env.DB_NAME,
-    process.env.DB_USER,
-    process.env.DB_PASSWORD,
+    process.env.DB_NAME_WRITE,
+    process.env.DB_USER_WRITE_WRITE,
+    process.env.DB_PASSWORD_WRITE,
     {
-        host: process.env.DB_HOST,
+        host: process.env.DB_HOST_WRITE,
         dialect: "postgres",
-        port: process.env.DB_PORT,
+        port: process.env.DB_PORT_WRITE,
     }
 );
