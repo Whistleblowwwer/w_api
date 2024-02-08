2fec760c422cebe156385a528ee7d4cf15898c8d
Alfonso García Yague <93610107+p0nch000@users.noreply.github.com>
2024-02-08 13:30:02 -0600

    Email domain verification added

--- utils/inputValidations.js
+++ utils/inputValidations.js
@@ -1,8 +1,17 @@
+import dns from 'dns/promises';
+
 export const isValidEmail = async (email) => {
     const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
     if (!regex.test(email)) return false;
 
-    return true;
+    const domain = email.split('@')[1];
+    try {
+        const mxRecords = await dns.resolveMx(domain);
+        return mxRecords && mxRecords.length > 0;
+    } catch (error) {
+        console.error(`Error resolving MX records for domain ${domain}:`, error);
+        return false;
+    }
 };
 
 export const isValidPhoneNumber = (phoneNumber) => {
